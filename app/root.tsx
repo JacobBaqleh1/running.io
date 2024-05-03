bccf23cdc9c92e440da2b734b664687c9af2ef86
Jacob Baqleh <baqlehjacob1@gmail.com>
2024-05-02 23:35:32 -0700

    set up a stopwatch

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: "tailwind.css" },
+];
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-
+import { useEffect, useState } from "react";
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -8,34 +8,93 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [timer, setTimer] = useState(false);
+  const [finish, setFinish] = useState(false);
+  const [start, setStart] = useState(true);
+  const [paragraphs, setParagraph] = useState([]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timer) {
+        setSecond((prevSecond: number) => {
+          return prevSecond + 1;
+        });
+      }
+      if (second === 59) {
+        setSecond((prevSecond: number) => {
+          return (prevSecond = 0);
+        });
+        setMinute((prevMinute) => {
+          return prevMinute + 1;
+        });
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timer, second]);
+
+  const pause = () => {
+    setTimer(false);
+  };
+  const resume = () => setTimer(true);
+  const startBtn = () => {
+    setTimer(true);
+    setFinish(true);
+    setStart(false);
+  };
+  const finishBtn = () => {
+    setParagraph((prevParagraph) => [
+      ...prevParagraph,
+      `${hour < 10 ? "0" + hour : hour}:${
+        minute < 10 ? "0" + minute : minute
+      }:${second < 10 ? "0" + second : second}`,
+    ]);
+
+    setTimer(false);
+    setStart(true);
+    setFinish(false);
+    setSecond(0);
+    setMinute(0);
+    setHour(0);
+  };
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main className="flex flex-col items-center justify-center h-screen">
+      {/* stopwatch */}
+      <div className="border-2 border-pink-500 flex flex-col items-center justify-center m-auto h-[20rem] w-[20rem] ">
+        {/* timer display */}
+        <div>
+          {hour < 10 ? 0 : ""}
+          {hour}:{minute < 10 ? 0 : ""}
+          {minute}:{second < 10 ? 0 : ""}
+          {second}
+        </div>
+        {/* start  */}
+        <div className={finish ? "hidden" : ""}>
+          <button onClick={startBtn}>Start</button>
+        </div>
+        {/* pause */}
+        <div>
+          <button onClick={pause}>Pause</button>
+        </div>
+        {/* resume */}
+        <div>
+          <button onClick={resume}>Resume</button>
+        </div>
+        {/* finish */}
+        <div className={start ? "hidden" : ""}>
+          <button onClick={finishBtn}>Finish</button>
+        </div>
+      </div>
+      {/* Time Results */}
+      <div>
+        {paragraphs.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
+    </main>
   );
 }
